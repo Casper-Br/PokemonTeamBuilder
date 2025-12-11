af6e8b45dfd671b4af2d6e41c099a1ac5471e7af
Casper-Br <casper-b@live.nl>
2025-12-11 18:02:07 +0100

    Added API functionality that fetches every pokemon available in Pokemon Sacred Gold

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const pokemon_API = 'https://pokeapi.co/api/v2/pokemon?limit=493&offset=0'
 const teamPokemons = document.querySelectorAll(".teamPokemon");
 let selectedItem = null;
+const pokemonDisplay = document.getElementById("pokemonDisplay");
 
 teamPokemons.forEach(teamPokemon => {
     teamPokemon.addEventListener("click", () => {
@@ -9,3 +11,25 @@ teamPokemons.forEach(teamPokemon => {
         selectedItem = teamPokemon;
     });
 });
+
+fetch(pokemon_API)
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(pokemon => {
+            const pokemonListElement = document.createElement('div');
+            pokemonListElement.innerText = pokemon.name;
+            pokemonListElement.classList.add('pokemonListElement');
+            pokemonDisplay.appendChild(pokemonListElement);
+        });
+    })
+    .catch(error => console.error("Error fetching Pokemon", error));
+
+    // Style fix, maybe figure out a way to do this in CSS
+    function matchPokedexHeight() {
+        const teamView = document.querySelector(".teamView");
+        const pokedex = document.querySelector(".pokedex");
+        pokedex.style.height = teamView.offsetHeight + "px";
+    }
+
+    window.addEventListener("load", matchPokedexHeight);
+    window.addEventListener("resize", matchPokedexHeight);
